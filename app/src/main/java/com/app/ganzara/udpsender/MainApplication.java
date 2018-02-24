4e5100ba0168840dfe0a3dc5063a024b30762dff
rhanza <rhanza@bk.ru>
2018-02-25 01:55:45 +0300

    *try make service immortal

--- app/src/main/java/com/app/ganzara/udpsender/MainApplication.java
+++ app/src/main/java/com/app/ganzara/udpsender/MainApplication.java
@@ -2,33 +2,20 @@ package com.app.ganzara.udpsender;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.BitmapFactory;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import static com.app.ganzara.udpsender.Constants.NOTIFY_ID;
 
 public class MainApplication extends Application {
 
     public MainActivity currentActivity;
     public Handler handler;
 
-    private volatile List<String> responses = Collections.synchronizedList(new ArrayList<String>());
+    public List<String> responses = Collections.synchronizedList(new ArrayList<String>());
 
     @SuppressLint("HandlerLeak")
     @Override
@@ -59,40 +46,5 @@ public class MainApplication extends Application {
         if (currentActivity != null) {
             currentActivity.addResponseNotify();
         }
-
-        if (sound) {
-            sendNotification(response);
-        }
     }
-
-    private void sendNotification(String response) {
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this,
-                0, notificationIntent,
-                PendingIntent.FLAG_CANCEL_CURRENT);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        NotificationCompat.Builder builder;
-
-        if (Build.VERSION.SDK_INT < 26) {
-            builder = new NotificationCompat.Builder(this);
-        } else {
-            NotificationChannel notificationChannel = new NotificationChannel("defaultId", "default", NotificationManager.IMPORTANCE_DEFAULT);
-            notificationManager.createNotificationChannel(notificationChannel);
-            builder = new NotificationCompat.Builder(this, "defaultId");
-        }
-
-        builder.setContentIntent(contentIntent)
-                .setSmallIcon(R.drawable.ic_notification)
-                .setContentTitle("Получен Response")
-                .setContentText(response)
-        .setDefaults( Notification.DEFAULT_SOUND |
-                Notification.DEFAULT_VIBRATE);
-
-        notificationManager.notify(NOTIFY_ID, builder.build());
-    }
-
-
 }
--- app/src/main/java/com/app/ganzara/udpsender/MainService.java
+++ app/src/main/java/com/app/ganzara/udpsender/MainService.java
@@ -1,31 +1,38 @@
 package com.app.ganzara.udpsender;
 
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.app.ganzara.udpsender.model.EndPoint;
 import com.app.ganzara.udpsender.model.UDPHelper;
 
 import java.io.IOException;
 
+import static com.app.ganzara.udpsender.Constants.NOTIFY_ID;
+
 public class MainService extends Service {
 
+    private static final String LOG_TAG = "SERVICE";
     private MainApplication application;
     private UDPHelper udp;
     private Thread work;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        application = (MainApplication) getApplication();
-        udp = new UDPHelper();
-    }
+    public Handler handler;
 
     @Nullable
     @Override
@@ -33,8 +40,24 @@ public class MainService extends Service {
         return null;
     }
 
+    @SuppressLint("HandlerLeak")
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG, "service started");
+        application = (MainApplication) getApplication();
+        udp = new UDPHelper();
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                Bundle data = msg.getData();
+                String message = data.getString(Constants.MESSAGE_KEY);
+                boolean sound = data.getBoolean(Constants.SOUND_KEY);
+                onResponse(message, sound);
+            }
+        };
+
         String ip = intent.getStringExtra(Constants.IP_KEY);
         final String message = intent.getStringExtra(Constants.MESSAGE_KEY);
         int port = intent.getIntExtra(Constants.PORT_KEY, 0);
@@ -49,8 +72,8 @@ public class MainService extends Service {
             public void run() {
                 while (true) {
                     try {
+                        Log.d(LOG_TAG, "response running");
                         String response = udp.sendAndWait(message, endPoint, interval);
-                        Handler handler = application.handler;
                         Message msg = handler.obtainMessage();
                         Bundle bundle = new Bundle();
                         bundle.putString(Constants.MESSAGE_KEY, response);
@@ -59,9 +82,14 @@ public class MainService extends Service {
                         handler.sendMessage(msg);
                         Thread.sleep(interval);
                     } catch (InterruptedException e) {
+                        Log.d(LOG_TAG, "response interrupt");
                         e.printStackTrace();
                         break;
                     } catch (IOException e) {
+                        Log.d(LOG_TAG, "response IO exception");
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        Log.d(LOG_TAG, "response exception");
                         e.printStackTrace();
                     }
                 }
@@ -70,12 +98,54 @@ public class MainService extends Service {
 
         work.start();
 
-        return super.onStartCommand(intent, flags, startId);
+        return START_REDELIVER_INTENT;
     }
 
     @Override
     public void onDestroy() {
+        Log.d(LOG_TAG, "service destroy");
         super.onDestroy();
         if (work != null) work.interrupt();
+    }
+
+    private void onResponse(String response, boolean sound) {
+//        if (application != null) {
+//            application.addResponse(response, sound);
+//        }
+
+        if (sound) {
+            sendNotification(response);
+        }
+    }
+
+    private void sendNotification(String response) {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, notificationIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder;
+
+        if (Build.VERSION.SDK_INT < 26) {
+            builder = new NotificationCompat.Builder(this);
+        } else {
+            NotificationChannel notificationChannel = new NotificationChannel("defaultId", "default", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(notificationChannel);
+            builder = new NotificationCompat.Builder(this, "defaultId");
+        }
+
+        Notification notification = builder.setContentIntent(contentIntent)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle("Получен Response")
+                .setContentText(response)
+                .setDefaults(Notification.DEFAULT_SOUND |
+                        Notification.DEFAULT_VIBRATE)
+                .setPriority(Notification.PRIORITY_MAX)
+                .build();
+
+        notificationManager.notify(NOTIFY_ID, notification);
     }
 }
